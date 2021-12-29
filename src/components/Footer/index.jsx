698bab73e35a594e73bcced7ce524d7e996263e4
Clzzi <clzzi1109@naver.com>
2021-12-29 13:47:11 +0900

    :sparkles: :: Footer
    풑

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import "./footer.scss";
+import { ReactComponent as Person } from "../../assets/person.svg";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="Footer">
+        <Person className="Footer-Person" />
+      </div>
+    </>
+  );
+};
+
+export default Footer;
