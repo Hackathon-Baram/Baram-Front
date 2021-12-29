0b0033d7583db1d99d713f7668c30baf2cc7768e
Clzzi <clzzi1109@naver.com>
2021-12-29 14:00:11 +0900

    :construction: :: card position
    카드 포지션

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,17 +1,17 @@
 import "./card.scss";
+import CardTitle from "../CardTitle";
 import { useRecoilState } from "recoil";
 import { orderAtom } from "../../atom/atom";
 import { ReactComponent as Heart } from "../../assets/heart.svg";
 import { ReactComponent as FillHeart } from "../../assets/heart-fill.svg";
-import CardTitle from "../CardTitle";
 
 const Card = () => {
   const [order, setOrder] = useRecoilState(orderAtom);
   const dumi = [
+    { idx: 0, text: "아니, 기숙사 아침운동 없애버려 주세요." },
     { idx: 1, text: "아니, 기숙사 아침운동 없애버려 주세요." },
     { idx: 2, text: "아니, 기숙사 아침운동 없애버려 주세요." },
     { idx: 3, text: "아니, 기숙사 아침운동 없애버려 주세요." },
-    { idx: 4, text: "아니, 기숙사 아침운동 없애버려 주세요." },
   ];
 
   const CardItem = dumi.map((item) => (
@@ -22,15 +22,15 @@ const Card = () => {
         </div>
         <div className="CardWrapper-Card-Text">{item.text}</div>
       </div>
-      {item.idx % 2 === 0 ? (
+      {Math.round(item.idx / 2) % 2 ? (
         <div
           className="CardWrapper-Bottom"
-          style={{ backgroundColor: "#87b9e7" }}
+          style={{ backgroundColor: "#8170FA" }}
         />
       ) : (
         <div
           className="CardWrapper-Bottom"
-          style={{ backgroundColor: "#8170FA" }}
+          style={{ backgroundColor: "#87b9e7" }}
         />
       )}
     </div>
