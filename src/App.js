c3559331fb0b5f0fef5ff535931d2e1717549c71
Clzzi <clzzi1109@naver.com>
2021-12-29 10:43:15 +0900

    :heavy_plus_sign: :: snowfall lib
    눈내리는 lib 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "../src/styles/reset.scss";
+import Snowfall from "react-snowfall";
 
 const App = () => {
-  return <div>Baram</div>;
+  return (
+    <>
+      <Snowfall />
+      <h1 color="black">DFAS</h1>
+    </>
+  );
 };
 
 export default App;
