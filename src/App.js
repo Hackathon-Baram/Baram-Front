143372a3bb25c924a7e1b13db6db06e6cde820ef
Clzzi <clzzi1109@naver.com>
2021-12-29 16:30:51 +0900

    :sparkles: :: card & input
    카드

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     <>
       <div className="wrapper">
         <Header />
-        {/* <Card /> */}
+        <Card />
       </div>
       <Footer />
       <Snowfall />
--- src/components/Editor/Button/Button.jsx
+++ src/components/Editor/Button/Button.jsx
@@ -1,17 +1,32 @@
-import axios from 'axios';
-import { toast } from 'react-toastify';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { addPostContent } from '../../../atom/atom';
-import '../Button/button.scss';
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { addPostContent, postAtom } from "../../../atom/atom";
+import "../Button/button.scss";
 
 const Button = () => {
+  const [posts, setPosts] = useRecoilState(postAtom);
   const [postContent, setPostContent] = useRecoilState(addPostContent);
+
+  const getPost = async () => {
+    try {
+      const { data } = await axios.get(`http://172.20.10.8:8080/post`, {
+        headers: { "Access-Control-Allow-Origin": "*" },
+      });
+      const t = data.data.sort((a, b) => {
+        return b.idx - a.idx;
+      });
+      setPosts(t);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   const addPost = async () => {
     try {
       if (postContent === "") {
-        toast.error('바람 등록 실패!', {
-          position: 'top-right',
+        toast.error("바람 등록 실패!", {
+          position: "top-right",
         });
 
         return;
@@ -21,8 +36,10 @@ const Button = () => {
         content: postContent,
       });
 
+      await getPost();
+
       if (response.data.status === 200) {
-        toast('😎 바람 등록 성공!', {
+        toast("😎 바람 등록 성공!", {
           position: "top-right",
           autoClose: 5000,
           hideProgressBar: false,
@@ -35,15 +52,17 @@ const Button = () => {
 
       setPostContent("");
     } catch (error) {
-      toast.error('바람 등록 실패!', {
-        position: 'top-right',
+      toast.error("바람 등록 실패!", {
+        position: "top-right",
       });
     }
-  }
+  };
 
   return (
-    <div className='Button'>
-      <button className="Button-wrap" onClick={addPost} type='submit'>등록하기</button>
+    <div className="Button">
+      <button className="Button-wrap" onClick={addPost} type="submit">
+        등록하기
+      </button>
     </div>
   );
 };
